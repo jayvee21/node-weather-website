e35f85ce7de51d0c4287a1a4253dae7bcfc15f23
Jayvee <jaygerero21@gmail.com.com>
2020-05-08 13:50:38 +0800

    prepare 80 port

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
 
+const port = process.env.PORT || 3000
 
 // Setup handle bars
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -88,6 +89,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log('api is running at port 3000')
 })
